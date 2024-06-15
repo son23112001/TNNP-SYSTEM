57debd265e8b410af6911ca65566de7848f45053
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2024-06-15 11:32:28 +0700

    Update obfuscated.lua

--- client/obfuscated.lua
+++ client/obfuscated.lua
@@ -1,3 +1,9 @@
 return function(script_key, func)
-    print(script_key, func)
+    print("Script Key: ", script_key)
+    print("Function: ", func)
+    if func and type(func) == "function" then
+        func()
+    else
+        print("No function provided or invalid function.")
+    end
 end
