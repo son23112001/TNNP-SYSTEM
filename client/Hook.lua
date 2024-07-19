e51c041c3015a9ed8acfcd84e0649960dbebb75f
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2024-07-19 00:03:41 -0700

    Delete client/Hook.lua

--- client/Hook.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game.ReplicatedStorage
-local GuideModule = require(ReplicatedStorage:WaitForChild("GuideModule")).ChangeDisplayedNPC
-local Death = require(ReplicatedStorage.Effect.Container.Death)
-local Respawn = require(ReplicatedStorage.Effect.Container.Respawn)
-hookfunction(GuideModule, function() return false end)
-hookfunction(Death, function()end)
-hookfunction(Respawn, function()end)
