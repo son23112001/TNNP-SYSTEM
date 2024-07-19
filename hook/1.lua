372441720689555650ad278f91d2de47704d36bd
RedGamer12 <vulizminecraft20@gmail.com>
2024-07-19 14:56:18 +0700

    Update 1.lua

--- hook/1.lua
+++ hook/1.lua
@@ -2,16 +2,6 @@ local GuideModule = require(game.ReplicatedStorage:WaitForChild("GuideModule")).
 local Death = require(game.ReplicatedStorage.Effect.Container.Death)
 local Respawn = require(game.ReplicatedStorage.Effect.Container.Respawn)
 
-local function disableHook(func, hookName)
-    local success, result = pcall(hookfunction, func, function() return false end)
-
-    if success then
-        print("TNNP successfully disabled "..hookName..".")
-    else
-        game.Players.LocalPlayer:Kick("Error disabling: '" .. hookName .. "'")
-    end
-end
-
-disableHook(GuideModule, "GuideModule")
-disableHook(Death, "Death")
-disableHook(Respawn, "Respawn")
+hookfunction(GuideModule, function() return false end)
+hookfunction(Death, function() return false end)
+hookfunction(Respawn, function() return false end)
